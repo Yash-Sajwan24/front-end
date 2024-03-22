3fd82319d8622a4f9a6e3ea04e627f4d239bdd53
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2024-03-22 14:53:45 +0530

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-'use client'
+"use client";
 import Spotlight from "@/components/collection-spotlight";
 import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
 import { createContext, useState, useContext } from "react";
 
-interface ThemeContextType {
-  isDarkTheme: boolean;
-  toggleTheme: () => void;
-}
-
-const ThemeContext = createContext<ThemeContextType | any>(undefined);
+const ThemeContext = createContext({
+  isDarkTheme: true,
+  toggleTheme: () => {},
+});
 
 export default function Home() {
   const [isDarkTheme, setIsDarkTheme] = useState(true);
 
   const toggleTheme = () => {
-    setIsDarkTheme(prevTheme => !prevTheme);
-    document.body.style.background = isDarkTheme ?  "#f7f7f8": "#292b32";
+    setIsDarkTheme((prevTheme) => !prevTheme);
+    document.body.style.background = isDarkTheme ? "#f7f7f8" : "#292b32";
   };
 
   return (
